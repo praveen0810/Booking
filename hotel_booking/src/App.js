568e1f97b9e79bbeff95a61c5488dfdbc5cd3b53
praveen <praveenhoswami61498@gmail.com>
2022-07-16 14:30:00 +0530

    all completed

--- hotel_booking/src/App.js
+++ hotel_booking/src/App.js
@@ -3,12 +3,19 @@ import Home from "./pages/Home";
 import { ChakraProvider } from "@chakra-ui/react";
 import Hotels from "./pages/Hotels";
 import Hotel from "./pages/Hotel";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <ChakraProvider>
-      {/* <Hotels /> */}
-      {/* <Home /> */}
-      <Hotel />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotels" element={<Hotels />} />
+          <Route path="/hotels/:id" element={<Hotel />} />
+          {/* <Route path="/login" element={<Login/>}/> */}
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
